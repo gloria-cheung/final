45d32ac74bef3c4195994d57b4ff54c6c8a0ae8a
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2022-09-26 15:29:38 -0600

    Create SellBottom.js

---
+++ react-front-end/src/components/Sell/SellBottom.js
@@ -0,0 +1,17 @@
+import React from "react";
+import classes from "./SellBottom.module.css";
+import { Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+
+export default function SellBottom() {
+  const navigate = useNavigate();
+  function clickHandler() {
+    navigate("/find-an-agent");
+  }
+  return (
+    <React.Fragment className={classes.container}>
+      <h1>Start Selling with an Agent</h1>
+      <Button onClick={clickHandler}>Find an Agent</Button>
+    </React.Fragment>
+  );
+}
