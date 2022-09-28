37b2cfb33dfbe5624d3e8edaa6b2de02f830b9af
Gloria <gloriacheung812@gmail.com>
2022-09-28 23:50:19 +0000

    extract req.body from react form to display into twilio msg

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -1,33 +1,32 @@
 const express = require("express");
-const app = express();
+const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const cors = require("cors");
+
+const twilio = require("twilio");
+require("dotenv").config();
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = new twilio(accountSid, authToken);
+
+const app = express();
 
 //middleware
 app.use(morgan("dev"));
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.post("/api/messages", (req, res) => {
   res.header("Content-Type", "application/json");
-  console.log(res);
-  const twilio = require("twilio");
-  require("dotenv").config();
-
-  const accountSid = process.env.TWILIO_ACCOUNT_SID;
-  const authToken = process.env.TWILIO_AUTH_TOKEN;
-
-  const client = new twilio(accountSid, authToken);
 
   client.messages
     .create({
-      body: "Hello from Node",
-      to: "+16478013218", // Text this number
+      body: `\nFrom: ${req.body.fullname}, \nPhone #: ${req.body.phone}, \nEmail: ${req.body.email}, \nListing ZPID: ${req.body.zpid} \nMessage: ${req.body.message}`,
+      to: "+16478013218", // in production, need to buy twilio numbers
       from: process.env.TWILIO_PHONE_NUMBER, // From a valid Twilio number
     })
     .then((message) => {
-      console.log(message);
       console.log("message sent");
       res.send(JSON.stringify({ success: true }));
     })
