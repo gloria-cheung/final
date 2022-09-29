47c691b4e99fb513e278f11d8bb06e662a5b9dc0
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2022-09-28 18:02:43 -0600

    Update FindAgent.js
    - add state to hold form information
    - add fetch data function to submit handler
    - pass props to other components

--- react-front-end/src/components/Sell/FindAgent.js
+++ react-front-end/src/components/Sell/FindAgent.js
@@ -1,48 +1,58 @@
-import React, { useRef, useState } from "react";
-import { Form, Card, Alert, Button } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import Axios from "axios";
+import React, { useState } from "react";
+
+import axios from "axios";
+// import classes from "./FindAgent.module.css";
+import SearchBarFindAgent from "./SearchBarFindAgent";
+import AgentsContainer from "./AgentsContainer";
 
 export default function FindAgent() {
-  const locationRef = useRef();
-  const agentNameRef = useRef();
+  const [location, setLocation] = useState("");
+  const [agentName, setAgentName] = useState();
+  const [specialties, setSpecialties] = useState("Any");
+  const [language, setLanguage] = useState("English");
+  const [data, setData] = useState({});
 
-  const [loading, setLoading] = useState(false);
-
-  function handleSubmit() {
+  const fetchData = () => {
     const options = {
       method: "GET",
       url: "https://zillow56.p.rapidapi.com/search_agents",
-      params: { location: locationRef, name: agentNameRef },
+      params: {
+        location: location,
+        name: agentName,
+        specialties: specialties,
+        language: language,
+      },
       headers: {
         "X-RapidAPI-Key": process.env.REACT_APP_API_KEY,
         "X-RapidAPI-Host": "zillow56.p.rapidapi.com",
       },
     };
-  }
+
+    axios
+      .request(options)
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    fetchData();
+  };
 
   return (
-    <>
-      <Card>
-        <Card.Body>
-          <h2 className="text-center mb-4">Find An Agent</h2>
-          <Form>
-            <Form.Group id="email">
-              <Form.Label>Location</Form.Label>
-              <Form.Control type="email" ref={locationRef} required />
-            </Form.Group>
-            <Form.Group id="password">
-              <Form.Label>Agent Name</Form.Label>
-              <Form.Control type="password" ref={agentNameRef} required />
-            </Form.Group>
-            <Button disabled={loading} className="w-100" type="submit">
-              Log In
-            </Button>
-          </Form>
-          <div className="w-100 text-center mt-3"></div>
-        </Card.Body>
-      </Card>
-      <div className="w-100 text-center mt-2"></div>
-    </>
+    <div>
+      <SearchBarFindAgent
+        setLocation={setLocation}
+        setAgentName={setAgentName}
+        setSpecialties={setSpecialties}
+        setLanguage={setLanguage}
+        submitHandler={submitHandler}
+      />
+      <AgentsContainer data={data} />
+    </div>
   );
 }
