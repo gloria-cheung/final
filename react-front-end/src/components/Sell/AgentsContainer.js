a346749cec7a0f26ed2d0408775b246616ecdc25
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2022-09-29 00:57:41 -0600

    map through data to render agentcontaineritem component

--- react-front-end/src/components/Sell/AgentsContainer.js
+++ react-front-end/src/components/Sell/AgentsContainer.js
@@ -1,8 +1,23 @@
 import React, { Fragment } from "react";
 import classes from "./AgentsContainer.module.css";
 import AgentsContainerItem from "./AgentsContainerItem";
+import { v4 as uuidv4 } from "uuid";
 
-export default function AgentsContainer() {
+export default function AgentsContainer({ data }) {
+  let agents;
+
+  if (Object.values(data).length !== 0) {
+    agents = data.map((agent) => (
+      <AgentsContainerItem
+        key={uuidv4()}
+        phoneNumber={agent.contact.summary.phone}
+        agentName={agent.profileLink.text}
+        agentGroup={agent.map.businessName}
+        mapImg={agent.map.image.src}
+        agentImg={agent.contact.graphic.image.src}
+      />
+    ));
+  }
   return (
     <React.Fragment>
       <div className={classes.container}>
@@ -11,16 +26,7 @@ export default function AgentsContainer() {
           <div className={classes.location}>Location</div>
           <div className={classes.profile}>Profile</div>
         </div>
-        <AgentsContainerItem />
-        <AgentsContainerItem />
-        <AgentsContainerItem />
-        <AgentsContainerItem />
-        <AgentsContainerItem />
-        <AgentsContainerItem />
-        <AgentsContainerItem />
-        <AgentsContainerItem />
-        <AgentsContainerItem />
-        <AgentsContainerItem />
+        {agents}
       </div>
     </React.Fragment>
   );
