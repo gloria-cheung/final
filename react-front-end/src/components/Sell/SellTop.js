d7a9c9fd03bde68137d1b1180d65e50a024780ab
Gloria <gloriacheung812@gmail.com>
2022-09-30 00:47:41 +0000

    add image overlay with text

--- react-front-end/src/components/Sell/SellTop.js
+++ react-front-end/src/components/Sell/SellTop.js
@@ -1,11 +1,23 @@
 import React from "react";
-import classes from "./SellTop.module.css";
+import { Card } from "react-bootstrap/";
+import "./SellTop.scss";
 
 export default function SellTop() {
   return (
-    <div className={classes.container}>
-      <h1>Sell Your Home with Confidence.</h1>
-      <h2>Home Finder makes it easy to sell your home. </h2>
-    </div>
+    <Card>
+      <Card.Img
+        className="sellTopImage"
+        src="https://images.unsplash.com/photo-1631888415289-f54436f81d8f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1055&q=80"
+        alt="cardBackground"
+      />
+      <Card.ImgOverlay>
+        <Card.Title className="sellTopTitle" as="h1">
+          Sell Your Home with Confidence.
+        </Card.Title>
+        <Card.Text className="sellTopText" as="h5">
+          Home Finder makes it easy to sell your home.
+        </Card.Text>
+      </Card.ImgOverlay>
+    </Card>
   );
 }
