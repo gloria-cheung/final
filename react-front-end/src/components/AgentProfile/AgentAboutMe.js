a52c2d15aae8d6e231e0f7937157bcca6bf9d3d6
Gloria <gloriacheung812@gmail.com>
2022-10-05 00:15:44 +0000

    use html react parse package to parse bio string

--- react-front-end/src/components/AgentProfile/AgentAboutMe.js
+++ react-front-end/src/components/AgentProfile/AgentAboutMe.js
@@ -1,10 +1,14 @@
 import React from "react";
+import parse from "html-react-parser";
+import "./AgentAboutMe.scss";
 
 export default function AgentAboutMe({ data }) {
   return (
     <>
       <h2>Bio</h2>
-      <div>{data ? data.about.description : null}</div>
+      <div className="agentBio">
+        {data ? parse(data.about.description) : null}
+      </div>
     </>
   );
 }
