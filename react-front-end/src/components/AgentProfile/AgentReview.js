2ff5028dac550df479509fc8c650c5d8f9f4ae59
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2022-10-06 02:03:08 -0600

    template for each review

--- react-front-end/src/components/AgentProfile/AgentReview.js
+++ react-front-end/src/components/AgentProfile/AgentReview.js
@@ -1,22 +1,61 @@
 import React from "react";
 
-export default function AgentReview() {
+import "./AgentReview.scss";
+
+export default function AgentReview(props) {
+  const { name, workDone, reviewBody } = props;
   return (
-    <React.Fragment>
-      <hr />
-      <h4>Highly likely to recommend | 5.0 ⭐️</h4>
-      <div>01/01/2020 - username</div>
-      <div>⭐️⭐️⭐️⭐️⭐️ Local knowledge</div>
-      <div>⭐️⭐️⭐️⭐️⭐️ Process expertise</div>
-      <div>⭐️⭐️⭐️⭐️⭐️ Responsiveness</div>
-      <div>⭐️⭐️⭐️⭐️⭐️ Negotiation skills</div>
-      <div className="mt-1">
-        Lick plastic bags attack curtains ð•„ð”¼ð•†ð•Ž wake up human for food at
-        4am. Plop down in the middle where everybody walks. Burrow under covers
-        i could pee on this if i had the energy but mouse and dead stare with
-        ears cocked so scratch my tummy actually i hate you now
+    <>
+      <div className="reviewContainer">
+        <h5>Highly likely to recommend | 5.0 ⭐️ </h5>
+        <div>{name}</div>
+        <div>{workDone}</div>
+        <br />
+        <div>⭐️⭐️⭐️⭐️⭐️ - Local Knowledge</div>
+        <div>⭐️⭐️⭐️⭐️⭐️ - Process Expertise</div>
+        <div>⭐️⭐️⭐️⭐️⭐️ - Responsiveness</div>
+        <div>⭐️⭐️⭐️⭐️⭐️ - Negotiation Skills</div>
+        <br />
+        <div className="reviewBody">{reviewBody}</div>
       </div>
-      <hr />
-    </React.Fragment>
+    </>
   );
+}
+
+{
+  /* <React.Fragment>
+      <hr />
+      <Row>
+        <h5>Highly likely to recommend | 5.0 ⭐️</h5>
+      </Row>
+      <Row>
+        <div>"reviewerDisplayName"</div>
+      </Row>
+      <Row>
+        <div>"revieweeWorkDone"</div>
+      </Row>
+      <Row className="mt-2">
+        <div>⭐️⭐️⭐️⭐️⭐️ - Local Knowledge</div>
+      </Row>
+      <Row>
+        <div>⭐️⭐️⭐️⭐️⭐️ - Process Expertise</div>
+      </Row>
+      <Row>
+        <div>⭐️⭐️⭐️⭐️⭐️ - Responsiveness</div>
+      </Row>
+      <Row>
+        <div>⭐️⭐️⭐️⭐️⭐️ - Negotiation Skills</div>
+      </Row>
+      <Row>
+        <div>
+          This is my second time I have sold a house with Katrina. She did an
+          outstanding job once again with communication, representation, and
+          marketing to ensure I had good exposure. By keeping a finger on the
+          pulse of our area real estate activity, she was able provide me with
+          professional sound advice to optimize the sale of my home. Would
+          highly recommend her services.
+        </div>
+      </Row>
+      <hr className="mt-2" />
+    </React.Fragment> */
 }
