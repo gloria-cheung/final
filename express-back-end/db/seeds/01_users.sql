754e520d381b2644f3e1c03b2db16dc076fa7330
gloria <macuser@MacUsers-MacBook-Pro.local>
2022-10-10 16:45:05 -0400

    removed dependencies not used and db

--- express-back-end/db/seeds/01_users.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO users (email, pw) VALUES ('billieeilish@gmail.com', 'blohsh');
-INSERT INTO users (email, pw) VALUES ('bob@gmail.com', 'password1');
-INSERT INTO users (email, pw) VALUES ('sara@gmail.com', 'password2');
-INSERT INTO users (email, pw) VALUES ('brian@gmail.com', 'password3');
-INSERT INTO users (email, pw) VALUES ('gloria@gmail.com', 'password4');
